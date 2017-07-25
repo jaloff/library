3cc5b73b1bd1e39ca4562d92f01be096094fe5a2
jalov <picpwnz@gmail.com>
2017-07-25 17:19:51 +0200

    Add hello world controller

--- src/main/java/jaloff/library/LibraryApplication.java
+++ src/main/java/jaloff/library/LibraryApplication.java
@@ -1,8 +1,10 @@
-package jaloff.library.library;
+package jaloff.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class LibraryApplication {
 
--- src/test/java/jaloff/library/LibraryApplicationTests.java
+++ src/test/java/jaloff/library/LibraryApplicationTests.java
@@ -1,4 +1,4 @@
-package jaloff.library.library;
+package jaloff.library;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
